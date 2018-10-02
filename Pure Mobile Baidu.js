b702af74112aebfd27464cace6245bdb3c26f892
Erbzur <aivenlyge@gmail.com>
2018-10-02 20:20:59 +0800

    Add files via upload

--- Pure Mobile Baidu.js
+++ Pure Mobile Baidu.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name			Pure Mobile Baidu
-// @version			1.5
+// @version			1.6
 // @description		Purify the shitty baidu search page on mobile devices
 // @editor			Erbzur
 // @include			*m.baidu.com*
@@ -10,12 +10,28 @@
 (function() {
 
     function purify() {
-        $('#index-card>div:not(#header):not(#bottom), .logined-banner, #header>#navs~*, #page-tips, #results>div:not([order]), [class*="c-recomm-wrap"], [tpl="recommend_list"], [class*="ec_"], .page-banner').remove();
+        //clean homepage
+        $('#index-card>div:not(#header):not(#bottom), .logined-banner, #header>#navs~*').remove();
+        if ($('#bottom')) {
+            $('body, #page, .navs-bottom-bar').css({
+                "background-color": "#fff"
+            });
+            $('#bottom').css({
+                "position": "relative",
+                "top": "310px",
+            });
+            $('#skin-wrap~*').css({
+                "position": "relative",
+                "top": "100px",
+            });
+        }
+        //clean results
+        $('#page-tips, #results>div:not([order]), [class*="c-recomm-wrap"], [tpl="recommend_list"], [class*="ec_"], .page-banner').remove();
         $('#page-ft').css({
-            display: "none !important"
+            "display": "none !important"
         });
         $('#page-copyright').css({
-            marginBottom: "0px !important"
+            "marginBottom": "0px !important"
         });
     }
 
