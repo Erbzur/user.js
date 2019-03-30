5a80fe303907e34c7dd198f91944a982883cab8d
Erbzur <aivenlyge@gmail.com>
2019-03-31 00:56:50 +0800

    iTunes HD Covers.js: first commit

---
+++ iTunes HD Covers.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name            iTunes HD Covers
+// @namespace       http://tampermonkey.net/
+// @version         1.0
+// @description     click on the cover to get the 1500x1500 picture.
+// @author          Erbzur
+// @include         *itunes.apple.com/ca/album/*
+// @grant           none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+	const covers = document.querySelectorAll('picture[class*="product"]');
+	const pixel = /\d+(?=x0w\.jpg$)/;
+	for (let ele of covers) {
+		try {
+			const coverURL = ele.querySelector('img').getAttribute('src');
+			ele.addEventListener('click', event => {
+				window.open(coverURL.replace(pixel, '1500'));
+			});
+			ele.style.cursor = 'pointer';
+		} catch (e) {}
+	}
+})();
